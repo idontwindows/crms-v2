a59b34b101a916f6ed71e968101adb4c154899b6
idontwindows <63185758+idontwindows@users.noreply.github.com>
2022-01-11 09:29:40 +0800

    Update index.php

--- backend/views/reports/index.php
+++ backend/views/reports/index.php
@@ -95,6 +95,53 @@ $this->params['breadcrumbs'][] = $this->title;
             </tr>
         </tbody>
     </table>
+    <div class="card">
+        <div class="card-header bg-primary text-white font-weight-bold">
+            Net Promoters
+        </div>
+        <table class="table ">
+            <thead class="thead-dark">
+                <tr>
+                    <!-- <th scope="col">#</th>
+                <th scope="col">Importance of Attribute</th> -->
+                    <th scope="col" class="text-md-center">1</th>
+                    <th scope="col" class="text-md-center">2</th>
+                    <th scope="col" class="text-md-center">3</th>
+                    <th scope="col" class="text-md-center">4</th>
+                    <th scope="col" class="text-md-center">5</th>
+                    <th scope="col" class="text-md-center">6</th>
+                    <th scope="col" class="text-md-center">7</th>
+                    <th scope="col" class="text-md-center">8</th>
+                    <th scope="col" class="text-md-center">9</th>
+                    <th scope="col" class="text-md-center">10</th>
+                </tr>
+            </thead>
+            <tbody ng-init="Fetchdata()">
+                <tr ng-repeat="report in reports.nps">
+                    <!-- <th scope="row">{{ $index + 1 }}</th> -->
+                    <!-- <td class="w-50 p-3">{{ report.question }}</td> -->
+                    <td class="text-md-center">{{ report.score1 }}</td>
+                    <td class="text-md-center">{{ report.score2 }}</td>
+                    <td class="text-md-center">{{ report.score3 }}</td>
+                    <td class="text-md-center">{{ report.score4 }}</td>
+                    <td class="text-md-center">{{ report.score5 }}</td>
+                    <td class="text-md-center">{{ report.score6 }}</td>
+                    <td class="text-md-center">{{ report.score7 }}</td>
+                    <td class="text-md-center">{{ report.score8 }}</td>
+                    <td class="text-md-center">{{ report.score9 }}</td>
+                    <td class="text-md-center">{{ report.score10 }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="d-flex justify-content-end" ng-init="Fetchdata()">
+        <div class="bg-info" ng-repeat="report in reports.nps">
+            <h4 class="ml-1 mr-1 text-white font-weight-bold">NPS: {{report.nps|number:0}}</h4>
+        </div>
+    </div>
+    
+    
+    
     <table class="table mt-5">
         <thead class="thead-dark">
             <tr>
