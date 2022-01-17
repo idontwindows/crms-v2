e8cd3df8d1dc343619497d1a030185b0a5a5362b
idontwindows <63185758+idontwindows@users.noreply.github.com>
2022-01-17 08:50:06 +0800

    Update _csf.php

--- frontend/views/site/_csf.php
+++ frontend/views/site/_csf.php
@@ -210,7 +210,7 @@ $this->registerCss('.modal-confirm {
                             <label for="select-gender"><b>Client type</b> (<span class="text-danger">Required</span>)</label>
                             <select name="customer_client_type" id="select-client-type" class="form-control">
                                 <option value="" disabled selected>Select Client type...</option>
-                                <option value="1">Gneneral Public</option>
+                                <option value="1">General Public</option>
                                 <option value="2">Internal Employees</option>
                                 <option value="3">Governement Employees</option>
                                 <option value="4">Businesses/Organization</option>
