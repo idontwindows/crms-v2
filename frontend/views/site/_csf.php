192a1d8ec37fd4a5eb374673e10f8a5596d2505b
idontwindows <63185758+idontwindows@users.noreply.github.com>
2022-01-19 16:24:51 +0800

    Update _csf.php

--- frontend/views/site/_csf.php
+++ frontend/views/site/_csf.php
@@ -149,6 +149,7 @@ $this->registerCss('.modal-confirm {
 //                             width: 300px;
 //                         }
 //                     }');
+
 ?>
 <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
