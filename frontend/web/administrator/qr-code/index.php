ec39726180dcec4b77da9aa637a765c9ea3f395b
idontwindows <63185758+idontwindows@users.noreply.github.com>
2022-07-29 16:51:20 +0800

    Create index.php

---
+++ frontend/web/administrator/qr-code/index.php
@@ -0,0 +1,32 @@
+<?php
+
+/* @var $this yii\web\View */
+/* @var $name string */
+/* @var $message string */
+/* @var $exception Exception */
+
+?>
+<div class="mb-5">
+                        <!-- <div class="card mb-3 mt-0 border-0 rounded shadow-lg"> -->
+                            <h1 class="ml-3 mt-3">Not Found (#404)</h1>
+                            <hr>
+
+                            <!-- <div class="card-body"> -->
+                                <div class="site-error">
+
+    <!-- <h1>Not Found (#404)</h1> -->
+
+    <div class="alert alert-danger">
+        Page not found.    </div>
+
+    <p>
+        The above error occurred while the Web server was processing your request.
+    </p>
+    <p>
+        Please contact us if you think this is a server error. Thank you.
+    </p>
+
+</div>
+                            <!-- </div> -->
+                        <!-- </div> -->
+                    </div>
